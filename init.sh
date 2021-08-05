d0d467da92cc79f2c1a727106fe0444870d21a9d
KitsuneCode <im.kitsunecode@gmail.com>
2021-08-04 20:14:06 -0500

    UwU

---
+++ init.sh
@@ -0,0 +1,11 @@
+if [[ ! -d code-server-3.11.0-linux-amd64 ]]
+then
+    wget https://github.com/cdr/code-server/releases/download/v3.9.3/code-server-3.11.0-linux-amd64.tar.gz
+    tar xvzf code-server-3.11.0-linux-amd64.tar.gz
+fi
+
+cd code-server-3.11.0-linux-amd64/bin
+
+# ./code-server --install-extension extension.id
+
+./code-server --bind-addr 0.0.0.0:8080 --auth password
